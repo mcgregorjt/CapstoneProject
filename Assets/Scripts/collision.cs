e7d47e22aa8af79f095f4b191eb708273ceaa4c1
Gabriel Interiano <89429215+gabecyber@users.noreply.github.com>
2023-02-17 18:55:45 -0500

    Add files via upload

---
+++ Assets/Scripts/collision.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy") {
+
+            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>().gameObject.GetComponent<BoxCollider2D>(), GetComponent<Collider2D>());
+        }
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
