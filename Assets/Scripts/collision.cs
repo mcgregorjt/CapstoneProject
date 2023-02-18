2b023c08463365f87ef9a88886d1f23f0ba5225a
Gabriel Interiano <89429215+gabecyber@users.noreply.github.com>
2023-02-18 17:43:04 -0500

    Add files via upload
    fixed some collision issues enemies had with the outer border, introduced a scrolling background.

--- Assets/Scripts/collision.cs
+++ Assets/Scripts/collision.cs
@@ -8,12 +8,16 @@ public class collision : MonoBehaviour
     void Start()
     {
         
+
     }
+
+        
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy") {
 
-            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>().gameObject.GetComponent<BoxCollider2D>(), GetComponent<Collider2D>());
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         }
         if (collision.gameObject.tag == "Bullet")
         {
