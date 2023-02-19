f253b67cf340b67a5fb517419aed0ac3930f7dd3
Gabriel Interiano <89429215+gabecyber@users.noreply.github.com>
2023-02-19 17:12:42 -0500

    Add files via upload
    fully implemented an outerbox for despawning enemies whenever they touch it. this helps with optimization, need to modify the enemy collision script with the inner wall.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -18,4 +18,14 @@ public class Bullet : MonoBehaviour
     {
         
     }
+
+    /*Contains collisio conditions fpr bullets*/
+    /*void OnCollisionEnter2D(Collision2D col)
+    {
+        Ignore items tagged as Item
+        if(col.gameObject.tag == "Item")
+        {
+            Physics2D.IgnoreCollision(GetComponent<CircleCollider2D>().gameObject.GetComponent<CircleCollider2D>(), GetComponent<Collider2D>());
+        }
+    }*/
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,4 +15,24 @@ public class Enemy : MonoBehaviour
     {
         
     }
+
+    /* Calculates damage received and subtracts it from enemy health.
+     * TODO: Currently only destroys enemy object when in contact with
+     *      enemy*/
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+            Death();
+        }
+    }
+
+    /* Destroys enemy object and potentially spawns an item */
+    void Death()
+    {
+        GetComponent<ItemSpawning>().ItemInstance(transform.position);
+        Destroy(this.gameObject);
+    }
 }
+
