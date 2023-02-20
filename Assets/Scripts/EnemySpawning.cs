f81fe6aeda2f0b7133cce4163d6a0ebd0b4862fa
Enday1 <elow300@gmail.com>
2023-02-20 00:44:02 -0500

    Updated made to script file
    Updated Enemy Spawning Script

--- Assets/Scripts/EnemySpawning.cs
+++ Assets/Scripts/EnemySpawning.cs
@@ -1,48 +1,132 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawning : MonoBehaviour {
-    //[SerializeField]public Rigidbody2D rb;
-    //[SerializeField]public Spriterenderer sr;
     
-    public GameObject enemy1Prefab;
-    //public GameObject[];
-    public Transform EnemySpawn;
-    public float xPos;
-    public float yPos;
-    public int count = 0;
-    public float startTimeDelay = 2;
-    public float timeInterval = 3;
+    //detail what enum does
+    public enum SpawnState { spawning, waiting, counting };
 
-    //Start is called before the first frame update
+    //Allows changes/edits to be made inside the inspector in Unity
+    [System.Serializable]
+
+    public class EnemyWave 
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+
+    }
+
+    public Transform[] spawnPoints;
+
+    public EnemyWave[] enemyWave;
+
+    //stores index of the next enemy
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+
+    private float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    public SpawnState state = SpawnState.counting;
+
     void Start() 
     {
-        InvokeRepeating("EnemySpawner", startTimeDelay, timeInterval);   
+        waveCountdown = timeBetweenWaves;
+
     }
 
-    // Update is called once perframe
-    void Update()
+    void Update() 
     {
-        {
-            EnemySpawner();
+        //used to see if enemies are still alive in game
+        if(state == SpawnState.waiting) {
+            //see if enemies are still alive
+            if(!enemyIsAlive()) {
+                waveCompleted();
+            }
+
+            else {
+                return;
+            }
+        }
+
+
+        if(waveCountdown <= 0) {
+            //checks to see whether or not to intialize the spawning of enemies
+            if(state != SpawnState.spawning) {
+                StartCoroutine(spawnWave(enemyWave[nextWave]));
+            }
+        }
+
+        else {
+            //time.deltaTime used to subract appropriate amount of time per frame for specific computer. This differs for different computers
+            //because, different computers have different frame rate processing speeds.
+            waveCountdown -= Time.deltaTime;
         }
     }
 
-    void EnemySpawner() 
+
+    void waveCompleted() 
     {
-       
 
-        if ( GameObject.FindGameObjectsWithTag("Enemy").Length < 3) {
+        Debug.Log("Wave Completed!");
+        state = SpawnState.counting;
+        waveCountdown = timeBetweenWaves;
 
-            
-            xPos = Random.Range(-1.5f, 2.49f);
-            yPos = Random.Range(5.98f, 6.50f);
-            Vector2 enemyPos = new Vector2(xPos, yPos);
-            Instantiate(enemy1Prefab, enemyPos, EnemySpawn.rotation);
-
-            //count += 1;
+        if(nextWave + 1 > enemyWave.Length - 1) {
+            nextWave = 0;
+            Debug.Log("All enemy waves complete. Looping");
         }
+
+        else {
+            nextWave++;
+        }
+        
+    }
+
+
+    bool enemyIsAlive() 
+    {
+        searchCountdown -= Time.deltaTime;
+        if(searchCountdown <= 0f) {
+
+            searchCountdown = 1f;
+            //very taxing piece of code on compute will check all game objects, thus the more game objects in the game the longer it will take
+            if(GameObject.FindGameObjectWithTag("Enemy") == null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //IEnumerator is used to wait a certain amount of seconds inside a method before continuing
+    IEnumerator spawnWave(EnemyWave wave) 
+    {
+        Debug.Log("Spawning Wave" + wave.name);
+        state = SpawnState.spawning;
+
+        for(int i = 0; i < wave.count; i++) {
+            spawnEnemy(wave.enemy);
+
+            yield return new WaitForSeconds(1f / wave.rate);
+        }
+
+        state = SpawnState.waiting;
+
+        yield break; // return nothing
+    }
+
+    void spawnEnemy(Transform enemy) 
+    {
+        Debug.Log("Spawning Enemy " + enemy.name);
+
+        int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+        //spawn points for enemy (may have to make random points on game plane
+        Instantiate(enemy, spawnPoints[randSpawnPoint].position, transform.rotation);
 
     }
 }
